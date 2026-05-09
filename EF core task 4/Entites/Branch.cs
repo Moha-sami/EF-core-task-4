56fc9d6ccf74a405ad3fe7ef7dfe6bd013166643
Moha-sami <engmohasami@gmail.com>
2026-05-10 00:33:26 +0300

    Add project files.

---
+++ EF core task 4/Entites/Branch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace EF_core_task_4.Entites
+{
+    public class Branch
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Code { get; set; } = null!; // e.g., NBG-CAI-01
+        public string Address { get; set; } = null!;
+
+        // Navigation Properties
+        public int? ManagerId { get; set; }
+        public virtual Manager? Manager { get; set; }
+
+        public virtual ICollection<Account> Accounts { get; set; } = new HashSet<Account>();
+    }
+
+
+}
+
---
+++ EF core task 4/Entites/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_core_task_4.Entites
+{
+    public class Manager
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string NationalId { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+
+        // Navigation Properties
+        public int BranchId { get; set; }
+        public virtual Branch Branch { get; set; } = null!;
+    }
+}
